69ffa7888aa40cf41784d6b7061611fca47a67b1
Thanh Tuan <tuandtse243@gmail.com>
2023-05-19 00:16:07 +0700

    Update api login

--- controllers/user.js
+++ controllers/user.js
@@ -146,4 +146,21 @@ router.get('/getuser', isAuthenticated, catchAsyncError(async (req, res, next) =
     }
 }))
 
+// log out user
+router.get('/logout', isAuthenticated, catchAsyncError(async (req, res, next) => {
+    try {
+        res.cookie('token', null, {
+            expires: new Date(Date.now()),
+            httpOnly: true,
+        });
+
+        res.status(201).json({
+            success: true,
+            message: 'Log out successful!'
+        })
+    } catch (error) {
+        return next(new ErrorHandler(error.message, 500));
+    }
+}))
+
 export default router
