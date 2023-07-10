5173bbaa9464f1e23ee16f03a8c6b3a873250f3f
Thanh Tuan <tuandtse243@gmail.com>
2023-07-10 15:48:29 +0700

    Merge production

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    // origin: 'https://s-store-frontend.vercel.app',
-    origin: 'http://localhost:3000',
+    origin: 'https://s-store-frontend.vercel.app',
+    // origin: 'http://localhost:3000',
     credentials: true,
 }))
 
